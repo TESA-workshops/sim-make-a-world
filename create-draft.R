8d030303feae5dda1efe1cd3ea5fefce8964d14b
ricardd <daniel.ricard@gmail.com>
2021-02-02 13:29:10 -0400

    tweaks

--- create-draft.R
+++ create-draft.R
@@ -1,5 +1,5 @@
 ## remotes::install_github("rstudio/rticles")
 
-rmarkdown::draft("otolith-ageing-article.Rmd", template = "acm", package = "rticles")
+rmarkdown::draft("otolith-ageing-article.Rmd", template = "plos", package = "rticles")
 
 
